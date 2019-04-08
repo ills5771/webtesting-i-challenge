3df5ae88d39afcdf994b424ed100d8fd4f445fd7
ills5771 <ilyayelizarov1@gmail.com>
2019-04-08 13:42:23 -0700

    adds repair() logic and test to repiar durability to 100

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,8 @@
 const enhancer = require("./enhancer.js");
 // test away!
+// test("repairs item to 100 durability", () => {
+//   expect(repair(55).durability.toEqual(100));
+// });
+test("repairs item to 100 durability", () => {
+  expect(enhancer.repair(55).durability).toBe(100);
+});
