586b423587f5a3cc64f14432c70754aa7ab7d767
ills5771 <ilyayelizarov1@gmail.com>
2019-04-08 15:49:12 -0700

    adds logic to succeed() and test to increase enhancement by 1

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,12 +6,28 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  let newItem = {
+    name: item.name,
+    enhancement: item.enhancement + 1,
+    durability: item.durability
+  };
+  return newItem;
 }
 
-function fail(item) {
-  return { ...item };
-}
+// function fail(item) {
+//   let { enhancement, durability } = item;
+//   if (enhancement < 15) {
+//     durability - 5;
+//   }
+
+//   return { ...item };
+
+// if (enhancement > 15) {
+//   durability - 10;
+// }
+// if (enhancement > 16) {
+//   enhancement - 1;
+// }
 
 function repair(item) {
   let newItem = {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -6,3 +6,26 @@ const enhancer = require("./enhancer.js");
 test("repairs item to 100 durability", () => {
   expect(enhancer.repair(55).durability).toBe(100);
 });
+
+it("increase enhancement by 1", () => {
+  expect(
+    enhancer.succeed({
+      enhancement: 10,
+      durability: 100
+    })
+  ).toEqual({
+    enhancement: 11,
+    durability: 100
+  });
+});
+// test("decrease durability by 5 if enhancement is less than 15", () => {
+//   expect(
+//     enhancer.fail({
+//       enhancement: 14,
+//       durability: 100
+//     })
+//   ).toEqual({
+//     enhancement: 14,
+//     durability: 95
+//   });
+// });
